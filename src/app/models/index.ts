293a156d82fe6b79ddca6921f98fdd08e29f3b1d
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-12 09:39:19 +0000

    Add models and db

---
+++ src/app/models/index.ts
@@ -0,0 +1,18 @@
+import { User } from './user';
+import { UserGame, GameRecord } from './user-game';
+import { Game } from './game';
+import { Platform } from './platform';
+import { Genre } from './genre';
+import { GameDetails, Screenshot, SystemRequirements } from './game-details';
+
+export {
+  type User,
+  type UserGame,
+  type Game,
+  type Platform,
+  type Genre,
+  type GameDetails,
+  type Screenshot,
+  type SystemRequirements,
+  type GameRecord,
+};
