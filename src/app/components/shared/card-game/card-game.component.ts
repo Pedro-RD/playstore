764cc0c01638419e29e4d6096d2d220f8432ec2d
João Monteiro <Joao.Monteiro.T0127542@edu.atec.pt>
2024-12-12 23:10:00 +0000

    card creation

--- src/app/components/shared/card-game/card-game.component.ts
+++ src/app/components/shared/card-game/card-game.component.ts
@@ -1,12 +1,19 @@
 import { Component, Input } from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
 import { Game } from '../../../models';
+import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-card-game',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardContent,
+    MatCard,
+    MatCardTitle
+  ],
   templateUrl: './card-game.component.html',
   styleUrl: './card-game.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardGameComponent {
   @Input({ required: true }) game!: Game;
