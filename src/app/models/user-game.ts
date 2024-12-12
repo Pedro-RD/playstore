21a791e49584f42d4c7fa68e9aa355aa451e2903
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-12 11:57:29 +0000

    Add navbar and api

--- src/app/models/user-game.ts
+++ src/app/models/user-game.ts
@@ -1,10 +1,15 @@
+// import { Game } from './game'; Verificar nested json-server
+import { User } from './user';
+
 export interface UserGame {
   id: string;
   userId: string;
   games: GameRecord[];
+  user?: User;
 }
 
 export interface GameRecord {
+  // game?: Game;
   gameId: string;
   createDate: string;
 }
