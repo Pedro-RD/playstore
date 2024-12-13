19a91ecdf7c73a2f1fc06d1a301c86df517a5a11
Henrique <henrique.gomes.t0127541@edu.atec.pt>
2024-12-13 16:10:17 +0000

    form criar player

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {MatError, MatFormField, MatHint, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MatButton, MatFabButton} from '@angular/material/button';
+
+class MyErrorStateMatcher extends ErrorStateMatcher {
+}
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatError,
+    MatHint,
+    MatIcon,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatFabButton,
+    MatButton
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  emailFormControl: FormControl = new FormControl('');
 
+  protected readonly FormControl = FormControl;
+  matcher: ErrorStateMatcher = new MyErrorStateMatcher();
+
+  clickEvent($event: MouseEvent) {
+
+  }
 }
