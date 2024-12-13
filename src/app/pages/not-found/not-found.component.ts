427ccd606e8cac210b9ea6fde95fcd0531cf3214
Angels <120145054+Angels4455@users.noreply.github.com>
2024-12-13 12:50:19 +0000

    create page not found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Para usar diretivas como *ngFor
+import { MatCardModule } from '@angular/material/card'; // Módulo do Material Design para Cards
+import { MatButtonModule } from '@angular/material/button'; // Módulo para Botões
+import { MatIconModule } from '@angular/material/icon'; // Importando MatIconModule
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule], // Importando o MatIconModule
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
+  aliens: number[] = []; // Array para gerar os alienígenas dinamicamente, você pode personalizar
 
+  constructor(private router: Router) {
+    this.generateAliens();
+  }
+
+  /**
+   * Redireciona o usuário para a página inicial
+   */
+  goBack(): void {
+    this.router.navigate(['/']); // Substitua '/' pela rota da sua página inicial
+  }
+
+  /**
+   * Gera um array para os alienígenas
+   */
+  private generateAliens(): void {
+    this.aliens = Array.from({ length: 5 }, (_, i) => i); // Gera 5 alienígenas para ficarem na página
+  }
 }
