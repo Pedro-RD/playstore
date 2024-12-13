a3abb56ac136e35c48903c76d1618f92178681b3
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-13 11:41:00 +0000

    Add mock user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { User } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private readonly loggedUser = new BehaviorSubject<User | undefined>({
+    id: '06c2',
+    name: 'Zé Povinho',
+    email: 'zpovinho@mail.pt',
+    password: '123456',
+    avatar:
+      'https://avatars.steamstatic.com/b32358b1a0f188ff07656a0c265b5af916224e96_full.jpg',
+  });
+
+  private readonly API_USERS = environment.apiUrl + 'users';
+
   constructor() {}
+
+  profile(): Observable<User | undefined> {
+    return this.loggedUser.asObservable();
+  }
+
+  isLogged(): Observable<boolean> {
+    return this.loggedUser
+      .asObservable()
+      .pipe(map((user) => user !== undefined));
+  }
 }
