9605c83a6e4fb631ffe3a4107839bfb7b7e47726
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-13 11:48:08 +0000

    Add profile info

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe, AsyncPipe],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  edit = false;
+  constructor(private authService: AuthService) {}
 
+  get profile() {
+    return this.authService.profile();
+  }
 }
