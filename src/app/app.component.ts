4cc8789552964a586cab4c7be877caffca9ba082
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-13 09:51:14 +0000

    integrate list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
-import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 import { LoadingService } from './services/loading.service';
 import { Subscription } from 'rxjs';
 
--- src/app/components/shared/card-game/card-game.component.ts
+++ src/app/components/shared/card-game/card-game.component.ts
@@ -1,16 +1,14 @@
 import { Component, Input } from '@angular/core';
-import {ChangeDetectionStrategy} from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Game } from '../../../models';
 import {
   MatCard,
   MatCardContent,
-  MatCardLgImage,
   MatCardTitle,
-  MatCardTitleGroup,
-  MatCardSubtitle, MatCardActions,
-
+  MatCardSubtitle,
+  MatCardActions,
 } from '@angular/material/card';
-import {MatAnchor} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card-game',
@@ -19,17 +17,14 @@ import {MatAnchor} from '@angular/material/button';
     MatCardContent,
     MatCard,
     MatCardTitle,
-    MatCardTitleGroup,
-    MatCardLgImage,
     MatCardSubtitle,
     MatCardActions,
-    MatAnchor
+    RouterLink,
   ],
   templateUrl: './card-game.component.html',
   styleUrl: './card-game.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardGameComponent {
   @Input({ required: true }) game!: Game;
-
 }
