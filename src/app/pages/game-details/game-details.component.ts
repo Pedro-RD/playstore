470ed800101257aa652eeb7c3bd7faf92dadf5dc
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-13 10:13:31 +0000

    Add details integration with service

--- src/app/pages/game-details/game-details.component.ts
+++ src/app/pages/game-details/game-details.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
-import { GeneralListComponent } from '../general-list/general-list.component';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { mergeMap, Subscription, tap } from 'rxjs';
+import { GameDetails } from '../../models';
+import { LoadingService } from '../../services/loading.service';
+import { GamesService } from '../../services/games.service';
+import { Logger } from '../../helpers/logger.helper';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-game-details',
   standalone: true,
-  imports: [GeneralListComponent],
+  imports: [JsonPipe],
   templateUrl: './game-details.component.html',
   styleUrl: './game-details.component.scss',
 })
-export class GameDetailsComponent {}
+export class GameDetailsComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+
+  game?: GameDetails;
+
+  constructor(
+    private route: ActivatedRoute,
+    private gamesService: GamesService,
+    private loadingService: LoadingService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadingService.show();
+    this.subscriptions.push(this.loadGameDetails());
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  private loadGameDetails() {
+    return this.route.params
+      .pipe(
+        tap((params) =>
+          Logger.log(
+            'GameDetailsComponent',
+            'Loading game details',
+            params['id']
+          )
+        ),
+        mergeMap((params) => this.gamesService.getGameDetails(params['id']))
+      )
+      .subscribe({
+        next: this.next,
+        error: this.error,
+      });
+  }
+
+  private next = (game: GameDetails) => {
+    this.game = game;
+  };
+  private error = (error: any) => {
+    // TODO
+  };
+}
