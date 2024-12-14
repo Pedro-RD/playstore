5ea7529dac78b47584cdd1d9e901f33cc6e1d899
Henrique <henrique.gomes.t0127541@edu.atec.pt>
2024-12-14 00:13:54 +0000

    form criar player

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {MatError, MatFormField, MatHint, MatLabel} from '@angular/material/form-field';
+import {MatError, MatFormField, MatHint, MatLabel, MatSuffix} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import {MatIcon} from '@angular/material/icon';
 import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
 import {MatButton, MatFabButton} from '@angular/material/button';
+import {AsyncPipe, NgClass, NgIf, NgOptimizedImage, NgStyle} from '@angular/common';
 
 class MyErrorStateMatcher extends ErrorStateMatcher {
 }
@@ -25,7 +26,13 @@ class MyErrorStateMatcher extends ErrorStateMatcher {
     MatDatepickerToggle,
     MatDatepicker,
     MatFabButton,
-    MatButton
+    MatButton,
+    AsyncPipe,
+    NgOptimizedImage,
+    NgIf,
+    NgStyle,
+    NgClass,
+    MatSuffix
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
@@ -35,8 +42,23 @@ export class RegisterComponent {
 
   protected readonly FormControl = FormControl;
   matcher: ErrorStateMatcher = new MyErrorStateMatcher();
+  profile: any = {};
+  file: string = '';
 
-  clickEvent($event: MouseEvent) {
+  onFileChange(event: any) {
+    const files = event.target.files as FileList;
 
+    if (files.length > 0) {
+      const _file = URL.createObjectURL(files[0]);
+      this.file = _file;
+      this.resetInput();
+    }
+
+  }
+  resetInput(){
+    const input = document.getElementById('avatar-input-file') as HTMLInputElement;
+    if(input){
+      input.value = "";
+    }
   }
 }
