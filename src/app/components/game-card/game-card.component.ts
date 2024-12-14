2ab06fbedf0a42b20cd0e314c9ecfdc998c52b0a
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-14 22:20:07 +0000

    Add buttons

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Game } from '../../models';
 import { RouterModule } from '@angular/router';
+import { GameUserActionsComponent } from '../game-user-actions/game-user-actions.component';
 
 @Component({
     selector: 'app-game-card',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, GameUserActionsComponent],
     templateUrl: './game-card.component.html',
     styleUrl: './game-card.component.scss',
 })
