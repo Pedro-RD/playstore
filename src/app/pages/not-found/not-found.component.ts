79fb72e74343b43d2f478b388a7417cbc1c699e6
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-12-15 09:32:33 +0000

    add 404

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+    selector: 'app-not-found',
+    standalone: true,
+    imports: [CommonModule, RouterLink],
+    templateUrl: './not-found.component.html',
+    styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
-
+    readonly pixels = Array(100).fill(0);
 }
